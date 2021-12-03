cb9334d6bc63253ca3b2adfc2ea8025e2f686b15
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2021-12-03 11:55:41 +0400

    reaction game by ninja

---
+++ src/components/Results.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="result" >
+        <p> Reaction time: {{score}} ms</p> 
+        <p class="rank">{{rank}}</p>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    props: ['score'],
+    data(){
+        return{
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 250 ) {
+            this.rank = 'ninja'
+        } else if (this.score < 400 ){
+            this.rank = "rapid reflexes"
+        } else {
+            this.rank = 'too slow bro'
+        }
+    }
+}
+</script>
+
+<style>
+    .result{
+        width: 150px;
+        padding: 30px;
+        border-radius: 20px;
+        background: #1619b9;
+        color: white;
+        text-align: center;
+        margin: 250px auto;
+
+    }
+    .rank{
+        font-size: 1.4em;
+        color: #0faf87;
+        font-weight: bold;
+    }
+
+
+
+</style>
